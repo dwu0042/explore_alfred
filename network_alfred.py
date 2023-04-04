3f9a6c089253781256560e5169249e27677bb786
dwu0042 <david.wu1@monash.edu>
2023-04-04 18:47:31 +1000

    add basic static network building

---
+++ network_alfred.py
@@ -0,0 +1,27 @@
+import overlap_residence as ovr
+import polars as pl
+
+import configparser
+env = configparser.ConfigParser()
+env.read('env.ini')
+
+def read(path=env['outputs']['clean_data']):
+    df = pl.read_csv(path, try_parse_dates=True)
+    return df
+
+def build_network(df:pl.DataFrame):
+    overlap_dict = ovr.compute_shared_residence_times(df.iter_rows())
+    G = ovr.make_overlap_graph(overlap_dict)
+    for nd, info in G.nodes(data=True):
+        if 'duration' in info:
+            G.nodes[nd]['duration'] = info['duration'].seconds/60/60  # -> hours
+    return G
+    
+def export_graph(G, export_name=env['outputs']['network']):
+    ovr.export_gml(G, f"{export_name}.gml")
+    ovr.export_graphviz(G, f"{export_name}.dot")
+
+if __name__ == "__main__":
+    df = read()
+    G = build_network(df)
+    export_graph(G)
