c1e305c59fab1328adaa15767abeeabf7b594f14
dwu0042 <david.wu1@monash.edu>
2023-04-19 20:52:45 +1000

    export viable overlaps, strict overlaps

--- find_cpe_tree.py
+++ find_cpe_tree.py
@@ -2,6 +2,7 @@
 
 import datetime
 import polars as pl
+from typing import Iterable
 from env_reader import env
 
 _end_of_period = datetime.date(year=2019, month=4, day=30)
@@ -29,8 +30,6 @@ def backsearch(individual, timing, overlaps):
             pl.col('patient1', 'patient2') == individual
         ) & (
             pl.col('overlap_start') < timing
-        ) & (
-            timing < pl.col('overlap_end')
         )
     )
 
@@ -42,12 +41,49 @@ def viable_overlaps(cpe_df: pl.DataFrame, overlaps: pl.DataFrame):
         for entry in cpe_df.iter_rows(named=True)
     }
 
+def export_viable_overlaps(overlaps: Iterable[pl.DataFrame], path: str=env['outputs']['backsearches']):
+    exportable = pl.concat(
+        (df
+            .with_columns(
+                sID=k, 
+                cause=(pl.col('patient1') + pl.col('patient2') - k)
+            ).select(
+                'sID',
+                'cause',
+                'fID',
+                'overlap_start',
+                'overlap_end',
+                'overlap_duration',
+            )
+        )
+        for k, df in overlaps.items()
+    )
+    exportable.write_csv(path)
+
+    missings = [k for k,df in overlaps.items() if df.shape[0] == 0]
+    missing_df = pl.from_dict(dict(missing=pl.Series(missings)))
+    missing_df.write_csv(f"{path}.missing")
+
+def strict_viable_overlaps(cpe_df: pl.DataFrame, viable_overlaps: pl.DataFrame):
+    return {
+        case: df.filter(
+            pl.any(pl.col('patient1', 'patient2').is_in(
+                set(cpe_df.select('sID').to_series().to_list()).difference({case})
+            ))
+        )
+        for case, df in viable_overlaps.items()
+    }
+
 if __name__ == "__main__":
-    import pickle
+    # import pickle
 
     cpe = load_cpe_data()
     cpe = clean_cpe_data(cpe)
     overlaps = load_overlaps()
     viables = viable_overlaps(cpe, overlaps)
-    with open("viable_overlaps.pkl", 'wb') as fp:
-        pickle.dump(viables, fp)
+    export_viable_overlaps(viables, env['outputs']['backsearches'])
+    strict_viables = strict_viable_overlaps(cpe, viables)
+    export_viable_overlaps(strict_viables, f"{env['outputs']['backsearches']}.strict")
+
+    # with open("viable_overlaps.pkl", 'wb') as fp:
+        # pickle.dump(viables, fp)
