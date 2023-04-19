d8f89b16178a9d09d172897cfcd551ac73f011d1
dwu0042 <david.wu1@monash.edu>
2023-04-19 17:25:25 +1000

    Sep overlap parsing from graph creation

--- network_alfred.py
+++ network_alfred.py
@@ -7,8 +7,31 @@ def read(path=env['outputs']['clean_data']):
     df = pl.read_csv(path, try_parse_dates=True)
     return df
 
-def build_network(df:pl.DataFrame):
-    overlap_dict = ovr.compute_shared_residence_times(df.iter_rows())
+def overlap_dict(df:pl.DataFrame):
+    return ovr.compute_shared_residence_times(df.sort('Adate').iter_rows())
+
+def build_overlaps_df(ov_dict):
+    overlap_rows = [
+        (this, other, location, duration.total_seconds()/60/60/24, start, end)
+        for this, this_info in ov_dict.items()
+        for location, loc_info in this_info.items()
+        for other, (duration, (start, end)) in loc_info.items()
+    ]
+    overlap_df = pl.from_records(overlap_rows, schema={
+        'patient1': pl.UInt32,
+        'patient2': pl.UInt32,
+        'ward': pl.Utf8,
+        'duration': pl.Float64,
+        'overlap_start': None,
+        'overlap_end': None,
+    })
+    return overlap_df
+
+def export_overlaps(ov_df: pl.DataFrame, path:str):
+    ov_df.write_csv(path)
+
+def build_network(overlap_dict):
+    # overlap_dict = ovr.compute_shared_residence_times(df.iter_rows())
     G = ovr.make_overlap_graph(overlap_dict)
     for nd, info in G.nodes(data=True):
         if 'duration' in info:
@@ -21,5 +44,8 @@ def export_graph(G, export_name=env['outputs']['network']):
 
 if __name__ == "__main__":
     df = read()
-    G = build_network(df)
-    export_graph(G)
+    ovl = overlap_dict(df)
+    ov_df = build_overlaps_df(ovl)
+    ov_df.sort('overlap_start').write_csv(env['output']['overlaps'])
+    # G = build_network(ovl)
+    # export_graph(G)
